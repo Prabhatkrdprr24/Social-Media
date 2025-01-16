7e83d47845cadc840619b9845c66e2ebb5618b2b
prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2025-01-16 18:37:38 +0530

    useEffect hook advance code implemented

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,24 +1,38 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
 
   const {postList, addInitialPosts} = useContext(PostListData);
-  
+  const [fetching, setFetching] = useState(false);
+
   useEffect(() => {
-    fetch('https://dummyjson.com/posts')
+    setFetching(true);
+
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch('https://dummyjson.com/posts', {signal})
       .then(res => res.json())
       .then(data => {
         addInitialPosts(data.posts);
+        setFetching(false);
       });
+
+      return () => {
+        console.log("Cleaning up UseEffect");
+        controller.abort();
+      };
   }, []);
   
     return (
         <>
-          {postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
-          {postList.map((post) => (
+          {fetching === true && <LoadingSpinner></LoadingSpinner>}
+          {fetching === false && postList.length === 0 && <WelcomeMessage></WelcomeMessage>}
+          {fetching === false && postList.map((post) => (
             <Post key={post.id} post = {post}/>
           ))}
         </>
